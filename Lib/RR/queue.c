5d44ef1975112ab1d13eea23da69e310d9c84c7a
tine <tine@gmail.com>
2023-10-13 13:21:54 +0200

    Working queue class buff of integers

--- Lib/RR/queue.c
+++ Lib/RR/queue.c
@@ -6,21 +6,25 @@
 void init_queue(queue_t *const self, int (*is_full_func)(queue_t const *self),
                 int (*is_empty_func)(queue_t const *self),
                 int (*get_size_func)(queue_t const *self),
-                int (*insert_element_func)(queue_t const *self),
-                int (*remove_element_func)(queue_t const *self)) {
+                void (*insert_element_func)(queue_t *const self,
+                                            const int element),
+                int (*remove_element_func)(queue_t *const self)) {
 
   self->head = 0;
   self->tail = 0;
   self->size = 0;
   // initialize member function pointers
-  self->is_full = is_full_cb;
+  self->is_full = is_full_func;
   self->is_empty = is_empty_func;
-  self->get_size = get_size_cb;
-  self->insert_element = insert_element_cb;
-  self->remove_element = remove_element_cb;
+  self->get_size = get_size_func;
+  self->insert_element = insert_element_func;
+  self->remove_element = remove_element_func;
 }
 
-void destroy_queue(queue_t const *self){};
+void destroy_queue(queue_t const *self) {
+  // TODO: rewrite and add destroy
+  printf("%d", (int *)self);
+};
 
 int is_full_cb(queue_t const *self) {
   return (self->head + 1) % QUEUE_MAX_SIZE == self->tail;
@@ -32,7 +36,11 @@ int get_size_cb(queue_t const *self) { return self->size; }
 
 void insert_element_cb(queue_t *const self, const int element) {
   if (!self->is_full(self)) {
+
+    printf("element in %d\n", element);
     self->buff[self->head] = element;
+
+    printf("element in %d\n", self->buff[self->head]);
     self->head = (self->head + 1) % QUEUE_MAX_SIZE;
     ++self->size;
   }
@@ -47,4 +55,12 @@ int remove_element_cb(queue_t *const self) {
   }
   // make return Some or None
   return value;
+}
+
+void print_all_elements_cb(queue_t *const self) {
+  if (!self->is_empty(self)) {
+    for (int idx = 0; idx < self->size; idx++) {
+      printf("idx: %d -> value: %d\n", idx, self->buff[self->tail + idx]);
+    }
+  }
 }
--- Lib/RR/queue.h
+++ Lib/RR/queue.h
@@ -11,8 +11,8 @@ struct queue_s {
   int tail;
   int size;
   int (*is_full)(queue_t const *self);
-  int (*is_empty)(queue_t *const self);
-  int (*get_size)(queue_t *const self);
+  int (*is_empty)(queue_t const *self);
+  int (*get_size)(queue_t const *self);
   void (*insert_element)(queue_t *const self, const int element);
   int (*remove_element)(queue_t *const self);
 };
@@ -21,8 +21,8 @@ struct queue_s {
 void init_queue(queue_t *const self, int (*is_full_func)(queue_t const *self),
                 int (*is_empty_func)(queue_t const *self),
                 int (*get_size_func)(queue_t const *self),
-                int (*insert_element_func)(queue_t *const self,
-                                           const int element),
+                void (*insert_element_func)(queue_t *const self,
+                                            const int element),
                 int (*remove_element_func)(queue_t *const self));
 
 void destroy_queue(queue_t const *self);
@@ -30,9 +30,10 @@ void destroy_queue(queue_t const *self);
 // Methods
 
 int is_full_cb(queue_t const *self);
-int is_empty_cb(queue_t *const self);
-int get_size_cb(queue_t *const self);
+int is_empty_cb(queue_t const *self);
+int get_size_cb(queue_t const *self);
 void insert_element_cb(queue_t *const self, const int element);
+void print_all_elements_cb(queue_t *const self);
 
 int remove_element_cb(queue_t *const self);
 

3bb4bd36a0978441621909023ff2b5aa1eaebeca
tine <tine@gmail.com>
2023-10-13 13:21:54 +0200

    Working queue class buff of integers

--- App/main.c
+++ App/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#include "queue.h"
 #include "sensor.h"
 
 int main(void) {
@@ -10,6 +11,24 @@ int main(void) {
   set_value(&sens_0, 99);
   printf("New value %d ", get_value(&sens_0));
   printf("End of program\n");
+
+  queue_t arr;
+  init_queue(&arr, is_full_cb, is_empty_cb, get_size_cb, insert_element_cb,
+             remove_element_cb);
+
+  printf("is full %d\n", arr.is_full(&arr));
+  if (arr.is_full(&arr)) {
+  }
+  for (int i = 0; i < 9; i++) {
+    insert_element_cb(&arr, i + 1);
+    printf("Get size %d\n", get_size_cb(&arr));
+    print_all_elements_cb(&arr);
+  }
+  for (int i = 0; i < 9; i++) {
+    remove_element_cb(&arr);
+    printf("Get size %d\n", get_size_cb(&arr));
+    print_all_elements_cb(&arr);
+  }
 
   return 0;
 }

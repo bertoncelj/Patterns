7a53ff1ec564f134ef0e50acc12a40aaedfcac2d
tine <tine@gmail.com>
2023-10-17 17:55:20 +0200

    queue person unit tests

--- Test/tests.c
+++ Test/tests.c
@@ -3,12 +3,19 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <cmocka.h>
 
 #include "queue.h"
 
 #define BUFFER_SIZE 10
+
+// Example usage:
+typedef struct {
+  uint8_t id;
+  char name[20];
+} Person;
 
 static void test_queue_uint8_size_10_all(void **state) {
   (void)state; // unused
@@ -64,9 +71,67 @@ static void test_queue_uint8_size_10_all(void **state) {
       remove_element(&q, &out_val)); // Should fail since queue is empty
 }
 
+// Test insertion and removal from the queue
+static void test_queue_size_person(void **state) {
+  (void)state; // unused
+
+  Person buffer[5 * sizeof(Person)];
+  queue_t queue;
+  init_queue(&queue, (uint8_t *)buffer, sizeof(Person), 5);
+
+  Person john = {1, "John"};
+  Person jane = {2, "Jane"};
+  Person output;
+
+  assert_int_equal(insert_element(&queue, &john), 1); // successful insert
+  assert_int_equal(insert_element(&queue, &jane), 1); // successful insert
+  assert_int_equal(is_empty(&queue), 0); // queue should not be empty
+
+  assert_int_equal(remove_element(&queue, &output), 1); // successful remove
+  assert_int_equal(output.id, john.id);                 // should be John
+  assert_string_equal(output.name, john.name);
+
+  assert_int_equal(remove_element(&queue, &output), 1); // successful remove
+  assert_int_equal(output.id, jane.id);                 // should be Jane
+  assert_string_equal(output.name, jane.name);
+}
+
+//
+// Test insertion and removal from the queue
+static void test_queue_size_person_all(void **state) {
+  (void)state; // unused
+
+  uint8_t buffer[10 * sizeof(Person)];
+  queue_t queue;
+  init_queue(&queue, (uint8_t *)buffer, sizeof(Person), 10);
+
+  Person persons[] = {{1, "John"},    {2, "Jane"},    {3, "Alice"},
+                      {4, "Bob"},     {5, "Charlie"}, {6, "Diana"},
+                      {7, "Edward"},  {8, "Fiona"},   {9, "George"},
+                      {10, "Hannah"}, {11, "Ian"},    {12, "Julia"}};
+  Person output;
+  for (uint8_t i = 0; i < 10 - 1; i++) {
+    assert_int_equal(insert_element(&queue, &persons[i]),
+                     1);                   // successful insert
+    assert_int_equal(is_empty(&queue), 0); // queue should not be empty
+  }
+  assert_int_equal(insert_element(&queue, &persons[9]), 0);
+  assert_int_equal(insert_element(&queue, &persons[10]), 0);
+  assert_int_equal(insert_element(&queue, &persons[11]), 0);
+
+  for (uint8_t i = 0; i < 10 - 1; i++) {
+
+    assert_int_equal(remove_element(&queue, &output), 1); // successful remove
+    assert_int_equal(output.id, persons[i].id);           // should be John
+    assert_string_equal(output.name, persons[i].name);
+  }
+}
+
 int main(void) {
   const struct CMUnitTest tests[] = {
       cmocka_unit_test(test_queue_uint8_size_10_all),
+      cmocka_unit_test(test_queue_size_person),
+      cmocka_unit_test(test_queue_size_person_all),
   };
   return cmocka_run_group_tests(tests, NULL, NULL);
 }

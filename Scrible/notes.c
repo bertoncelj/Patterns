a061ceae27a5074e009549b8fd0e952fba0957bb
tine <tine@gmail.com>
2023-10-18 18:01:54 +0200

    Event lib start concept

--- Scrible/notes.c
+++ Scrible/notes.c
@@ -1,11 +1,52 @@
 #include <stdio.h>
 
-#include "animal.h"
-#include "dog.h"
+#include "event.h"
+#include "queue.h"
 
-int main(int argc, char *argv[]) {
+// add + 1
+#define MAX_NUM_COMMANDS 10
 
-  printf("Hello World!\n");
+// Global
+uint8_t buffer_g[MAX_NUM_COMMANDS * sizeof(commands_t)];
+queue_t queue_g;
+
+void input(void) {
+
+  // neki neki
+  printf("input\n");
+  // neki neki
+}
+
+void output(void) {
+
+  // neki neki
+  printf("output\n");
+  // neki neki
+}
+
+int main() {
+  // Define the event table
+  struct commands_s event_table[20] = {
+      {"led1 toggle", "<led_on_1>", 0, foo1},
+      {"led2 toggle", "<led_on_2>", 0, foo2},
+      {"led3 toggle", "<led_on_3>", 0, foo3},
+      {"led4 toggle", "<led_on_4>", 0, foo4},
+      {"led5 toggle", "<led_on_5>", 0, foo5},
+  };
+
+  // init
+  init_queue(&queue_g, (uint8_t *)buffer_g, sizeof(commands_t),
+             MAX_NUM_COMMANDS);
+
+  for (uint8_t i = 0; i < 5; i++) {
+    insert_element(&queue_g, &event_table[i]);
+  }
+  uint8_t loop = 5;
+  while (loop--) {
+    input();
+    event_handler(&queue_g);
+    output();
+  }
 
   return 0;
 }

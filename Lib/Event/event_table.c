cf88a018e2ed8fe6bc18a2862a9af6de252da538
tine <bertotine@gmail.com>
2023-11-04 15:55:09 +0000

    task adc added

--- Lib/Event/event_table.c
+++ Lib/Event/event_table.c
@@ -1,10 +1,17 @@
 #include "event_table.h"
+#include "task_adc.h"
 #include "task_led.h"
 
 struct commands_s avaiable_event_table[20] = {
+    // LED
     {"descrip1", "run_led", 0, led_task, RUN},
     {"descrip2", "tog_led", 0, led_task, TOGGLE},
-    {"descrip4", "stop_led", 0, led_task, STOP}
+    {"descrip4", "stop_led", 0, led_task, STOP},
+
+    // ADC
+    {"descrip5", "run_adc", 0, adc_task, RUN},
+    {"descrip6", "stop_adc", 0, adc_task, STOP},
+    {"descrip7", "tog_adc", 0, led_task, TOGGLE},
     // {"description2", "cmd2", 0, foo2},
     // {"description3", "cmd3", 0, foo3},
     // {"description4", "cmd4", 0, foo4},
---
+++ Lib/Tasks/task_adc.h
@@ -0,0 +1,6 @@
+#ifndef _task_adc_h_
+#define _task_adc_h_
+#include "event.h"
+
+event_t adc_task(input_state_t input_state);
+#endif
